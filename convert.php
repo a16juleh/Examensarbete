7fd4073dc67e301c71e7939b77e5398c023e9cbe
a16juleh <a16juleh@student.his.se>
2019-03-12 16:07:52 +0100

    split Timestamp and HTTPandProtocolV keys into several keys

--- convert.php
+++ convert.php
@@ -3,23 +3,41 @@
     <body>
         <pre>
             <?php
-                $File = file('log.txt');
+                $File = file('../Access-log.txt'); 
                 $Array = array();
 
                 foreach ($File as $Line) {
                     $Object = new \stdClass();
-                    // Split the line and get the first element, the IP address
+                    // Split the line and get the first element, the IP-address
                     $Parts = explode(' ',trim($Line));
                     $Object->IPaddress = $Parts[0];
 
-                    // Get the value between two brackets, without saving the brackets
-                    $Firstpos=strpos($Line, "[") + 1;
-                    $Secondpos=strpos($Line, "]");
-                    $Object->Timestamp = substr($Line, $Firstpos, $Secondpos - $Firstpos);
+                    // Get the timestamp between two brackets
+                    $Firstpos = strpos($Line, "[") + 1;
+                    $Secondpos= strpos($Line, "]");
+                    $Timestamp = substr($Line, $Firstpos, $Secondpos - $Firstpos);
+
+                    // Split the timestamp and get the date 
+                    $TimestampParts = explode(':',trim($Timestamp));
+                    $Date = $TimestampParts[0];
+                    $Date = str_replace('/', '-', $Date);
+                    $Object->Date = date('Y-m-d', strtotime($Date));
+                    
+                    // Split the timestamp and get the time
+                    $Firstpos = strpos($Timestamp, ":") + 1;
+                    $Secondpos= strpos($Timestamp, " ");
+                    $Object->Time = substr($Timestamp, $Firstpos, $Secondpos - $Firstpos);
+
+                    // Split the timestamp and get the time zone 
+                    $TimeZone = explode(' ',trim($Timestamp));
+                    $Object->TimeZone = $TimeZone[1];                  
 
                     // Split the line and get the HTTP command and Protocol Version 
                     $Parts = explode('"',trim($Line));
-                    $Object->HTTPandProtocolV = $Parts[1];
+                    $HTTPandProtocolV = $Parts[1];
+                    $HTTPandProtocolVParts = explode(" ", $HTTPandProtocolV, 2);
+                    $Object->HTTPCommand = $HTTPandProtocolVParts[0];
+                    $Object->ProtocolVersion = $HTTPandProtocolVParts[1];
 
                     // Split the part after HTTP command and Protocol Version to get
                     // both Status Code and Bytes Transferred
@@ -36,13 +54,10 @@
                 }
                    
                 // Encode the array with all the objects into a JSON string
-                $JSONstring = json_encode($Array, JSON_PRETTY_PRINT);
-                
-                // Print out the JSON string
-                echo $JSONstring;
+                $JSONstring = json_encode($Array);
 
                 // Save the JSON string to a JSON file
-                file_put_contents('Access-log.json', $JSONstring);
+                file_put_contents('../test.json', $JSONstring);
             ?>
         </pre>
     </body>
