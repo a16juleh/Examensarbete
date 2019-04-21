aa74aef65c2cc66817d6c03eb4d1ab766ef6417d
a16juleh <a16juleh@student.his.se>
2019-04-22 01:24:02 +0200

    fix interactivity functionality and structure the code

--- googleChart-scatterChart.js
+++ googleChart-scatterChart.js
@@ -1,58 +1,56 @@
-var MAX = 31;
-var zoomed = false;
+var data, day, MAX = 31, zoomed = false;
 var options = {
-    title: 'Day vs Bytes Transferred',
+    title: 'Day vs StatusCode',
     hAxis: {title: 'Day', viewWindow: {min:0, max: MAX}},
-    vAxis: {title: 'BytesTransferred', viewWindow: {min:100, max: 10000}},
+    vAxis: {title: 'StatusCode', viewWindow: {min:100, max: 500}},
     legend: 'none',
     pointSize: 5,
     dataOpacity: 0.4,
-    trendlines: {
-        0: {
-          type: 'exponential',
-          visibleInLegend: true,
-        }
-      }  
 };
 
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+$(document).on ("click", "#Run", function () {
+    localStorage.setItem('startTime', new Date().getTime());
+    google.load('visualization', '1', {'packages':['corechart']});
+    google.setOnLoadCallback(getJSON);
+    document.getElementById('buttonChange').disabled = true;
+ });
 
-function drawChart() {
-    document.getElementById('Zoom').disabled = true;
-    var data, day;
+function getJSON(){
     $.ajax({
-    url: "test.json",
-    dataType: "JSON"
+        url: "mounth1-dec-2015.json",
+        dataType: "JSON"
     }).done(function(objects) {
         data = new google.visualization.DataTable();
         data.addColumn('number', 'Day');
-        data.addColumn('number', 'BytesTransferred');
-
+        data.addColumn('number', 'StatusCode');
+        
         for(var i = 0; i < objects.length; i++){
             day = (new Date((objects[i].Date)));
-            data.addRow([day.getDate(), parseInt(objects[i].BytesTransferred)]);
+            data.addRow([day.getDate(), parseInt(objects[i].StatusCode)]);
         }
-
-        var chart = new google.visualization.ScatterChart(document.getElementById('diagram'));
-        google.visualization.events.addListener(chart, 'ready',
-        function() {
-            document.getElementById('Zoom').disabled = false;
-        });
-        
-        chart.draw(data, options);
-
-        document.getElementById('Zoom').onclick = function() {
-            if (zoomed == false) {
-                options.hAxis.viewWindow.min = 30;
-                options.hAxis.viewWindow.max = 31;
-                zoomed == true;
-            } else {
-                options.hAxis.viewWindow.min = 0;
-                options.hAxis.viewWindow.max = MAX;
-            }
-            zoomed = !zoomed;
-            drawChart();
-        };
-    });  
+        drawChart();
+    });
 }
+
+function drawChart() {
+    var chart = new google.visualization.ScatterChart(document.getElementById('Diagram'));
+    google.visualization.events.addListener(chart, 'ready',
+    function() {
+        document.getElementById('buttonChange').disabled = false;
+    });
+    chart.draw(data, options);  
+    localStorage.setItem('stopTime', new Date().getTime());
+    localStorage.setItem('renderingTime', (localStorage.getItem('stopTime') - localStorage.getItem('startTime')));
+    console.log("Total rendering time: " + localStorage.getItem('renderingTime') + "ms"); 
+}
+
+$(document).on ("click", "#buttonChange", function () {
+    //localStorage.setItem('startTime', new Date().getTime());
+    options.hAxis.viewWindow.min = document.getElementById('xlimStart').value;
+    options.hAxis.viewWindow.max = document.getElementById('xlimEnd').value;
+        
+    drawChart();   
+    /* localStorage.setItem('stopTime', new Date().getTime());
+    localStorage.setItem('renderingTime', (localStorage.getItem('stopTime') - localStorage.getItem('startTime')));
+    console.log("Total rendering time: " + localStorage.getItem('renderingTime') + "ms");  */
+});
